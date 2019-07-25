5177029b37be202799efa81a9611103bad62460b
Dimitar Zhechev <dimityr.jechev@gmail.com>
2019-07-25 09:59:43 +0300

    Remove author component

--- Angular 8/books-app/src/app/app.module.ts
+++ Angular 8/books-app/src/app/app.module.ts
@@ -12,17 +12,20 @@ import {
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
+
 import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
 import { RegisterComponent } from './register/register.component';
+
 import { BookListComponent } from './book/book-list/book-list.component';
 import { BookDetailComponent } from './book/book-detail/book-detail.component';
-import { AddCommentComponent } from './comment/add-comment/add-comment.component';
-import { CommentDetailComponent } from './comment/comment-detail/comment-detail.component';
 import { AddBookComponent } from './book/add-book/add-book.component';
 import { BookEditComponent } from './book/book-edit/book-edit.component';
 import { BookByUserComponent } from './book/book-by-user/book-by-user.component';
-import { AuthorComponent } from './author/author.component';
+
+import { AddCommentComponent } from './comment/add-comment/add-comment.component';
+import { CommentDetailComponent } from './comment/comment-detail/comment-detail.component';
+
 import { MessageSendComponent } from './messages/message-send/message-send.component';
 import { InboxComponent } from './messages/inbox/inbox.component';
 import { ConversationComponent } from './messages/conversation/conversation.component';
