9912332dfe762e02adb358742b0cba9799ec0785
Dimitar Zhechev <dimityr.jechev@gmail.com>
2019-08-28 14:49:36 +0300

    Added pagination and style

--- React/album-app/src/components/Album/AlbumList/AlbumList.js
+++ React/album-app/src/components/Album/AlbumList/AlbumList.js
@@ -1,19 +1,47 @@
 import React, { Component } from 'react';
-import axios from '../../../axios-orders';
+import axios from 'axios';
 import Spinner from '../../UI/Spinner/Spinner';
 import Album from '../Album';
 import './AlbumList.css';
+import Pagination from 'react-js-pagination';
 
 class AlbumList extends Component {
-  state = {
-    loading: true,
-    albums: null
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      albums: null,
+      activePage: 1,
+      totalItems: null
+    };
+    this.handlePageChange = this.handlePageChange.bind(this);
+  }
 
   componentDidMount() {
     axios
-      .get()
+      .get(
+        'http://localhost:3000/albums?_page=' +
+          this.state.activePage +
+          '&_limit=5'
+      )
       .then(res => {
+        console.log(res);
+        this.setState({ totalItems: res.headers['x-total-count'] });
+        this.setState({ loading: false });
+        this.setState({ albums: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  handlePageChange(pageNumber) {
+    console.log(`active page is ${pageNumber}`);
+    this.setState({ activePage: pageNumber });
+    axios
+      .get('http://localhost:3000/albums?_page=' + pageNumber + '&_limit=5')
+      .then(res => {
+        console.log(res);
         this.setState({ loading: false });
         this.setState({ albums: res.data });
       })
@@ -23,6 +51,7 @@ class AlbumList extends Component {
   }
 
   render() {
+    const that = this;
     if (this.state.albums) {
       let albums = this.state.albums.map(album => {
         return (
@@ -45,6 +74,15 @@ class AlbumList extends Component {
             <div className='td'>Go to Album</div>
           </div>
           {albums}
+          <div className='paginationContainer'>
+            <Pagination
+              activePage={this.state.activePage}
+              itemsCountPerPage={5}
+              totalItemsCount={this.state.totalItems}
+              pageRangeDisplayed={5}
+              onChange={that.handlePageChange}
+            />
+          </div>
         </div>
       );
     } else {
